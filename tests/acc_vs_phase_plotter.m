551843c7582eb14d85d43596cd0354147613d6b6
simongg <simonllpgg@gmail.com>
2019-11-11 12:01:04 -0500

    did some good work on loss imbalance, good figures

--- tests/acc_vs_phase_plotter.m
+++ tests/acc_vs_phase_plotter.m
@@ -1,7 +1,10 @@
 clear; clc; close all;
 
 % Matlab plotter for Acc vs Phase inacc and loss
-FOLDER = 'Accuracy_vs_phaseUncert_300iter/';
+FOLDER = 'Accuracy_vs_phaseUncert&loss_400iter_100%acc2_butOnlyforRiR/';
+FOLDER = 'Accuracy_vs_phaseUncert&loss_500iter_100%acc/';
+% FOLDER = 'Accuracy_vs_phaseUncert_loss_500iter_100%acc/';
+
 phase_uncert = load([FOLDER, 'PhaseUncert.txt']);
 loss_dB = load([FOLDER, 'LossdB.txt']);
 
@@ -9,42 +12,78 @@ Reck = load([FOLDER, 'accuracy_Reck.txt']);
 invReck = load([FOLDER, 'accuracy_invReck.txt']);
 Reck_invReck = load([FOLDER, 'accuracy_Reck+invReck.txt']);
 Reck_DMM_invReck = load([FOLDER, 'accuracy_Reck+DMM+invReck.txt']);
+Reck_DMM = load([FOLDER, 'accuracy_Reck+DMM.txt']);
 
 % create legend elements
 legend_ = cell(length(loss_dB),1);
-for ii = 1:length(loss_dB)
-    legend_{ii} = sprintf('Loss = %.2f dB', loss_dB(ii));
+legend_ = cell(round(length(loss_dB)/4),1);
+for ii = 1:4:length(loss_dB)
+    legend_{round(ii/4)+1} = sprintf('Loss = %.2f dB', loss_dB(ii));
 end
 
-% Plot Reck Acc
-plot(phase_uncert, Reck, 'linewidth',2)
-legend(legend_);
-xlabel('Phase Uncertainty (\sigma)')
-ylabel('Accuracy (%)')
-title(sprintf('Accuracy VS Phase Uncertainty\n Reck layer\n 10 iterations'))
+if 1
+    % Plot Reck Acc
+    plot(phase_uncert, Reck(:, 1:4:end), 'linewidth',2)
+    legend(legend_);
+    xlabel('Phase Uncertainty (\sigma)')
+    ylabel('Accuracy (%)')
+    title(sprintf('Accuracy VS Phase Uncertainty\n Reck layer\n 500 iterations per Phase Uncertainty'))
+    
+    % Plot invReck Acc
+    figure
+    plot(phase_uncert, invReck(:, 1:4:end), 'linewidth',2)
+    legend(legend_);
+    xlabel('Phase Uncertainty (\sigma)')
+    ylabel('Accuracy (%)')
+    title(sprintf('Accuracy VS Phase Uncertainty\n Inverted Reck layer\n 500 iterations per Phase Uncertainty'))
+    
+    % Plot Reck+invReck Acc
+    figure
+    plot(phase_uncert, Reck_invReck(:, 1:4:end), 'linewidth',2)
+    legend(legend_);
+    xlabel('Phase Uncertainty (\sigma)')
+    ylabel('Accuracy (%)')
+    title(sprintf('Accuracy VS Phase Uncertainty\n Reck + Inverted Reck layer\n 500 iterations per Phase Uncertainty'))
+    
+    % Plot Reck+DMM+invReck Acc
+    figure
+    plot(phase_uncert, Reck_DMM_invReck(:, 1:4:end), 'linewidth',2)
+    legend(legend_);
+    xlabel('Phase Uncertainty (\sigma)')
+    ylabel('Accuracy (%)')
+    title(sprintf('Accuracy VS Phase Uncertainty\n Reck + DMM + Inverted Reck layer\n 500 iterations per Phase Uncertainty'))
+    
+    % Plot Reck+DMM Acc
+    figure
+    plot(phase_uncert, Reck_DMM(:, 1:4:end), 'linewidth',2)
+    legend(legend_);
+    xlabel('Phase Uncertainty (\sigma)')
+    ylabel('Accuracy (%)')
+    title(sprintf('Accuracy VS Phase Uncertainty\n Reck + DMM layer\n 500 iterations per Phase Uncertainty'))
+end
 
-% Plot invReck Acc
-figure
-plot(phase_uncert, invReck, 'linewidth',2)
-legend(legend_);
-xlabel('Phase Uncertainty (\sigma)')
-ylabel('Accuracy (%)')
-title(sprintf('Accuracy VS Phase Uncertainty\n Inverted Reck layer\n 10 iterations'))
-
-% Plot Reck+invReck Acc
-figure
-plot(phase_uncert, Reck_invReck, 'linewidth',2)
-legend(legend_);
-xlabel('Phase Uncertainty (\sigma)')
-ylabel('Accuracy (%)')
-title(sprintf('Accuracy VS Phase Uncertainty\n Reck + Inverted Reck layer\n 10 iterations'))
-
-% Plot Reck+DMM+invReck Acc
-figure
-plot(phase_uncert, Reck_DMM_invReck, 'linewidth',2)
-legend(legend_);
-xlabel('Phase Uncertainty (\sigma)')
-ylabel('Accuracy (%)')
-title(sprintf('Accuracy VS Phase Uncertainty\n Reck + DMM + Inverted Reck layer\n 10 iterations'))
-
-
+% % plot no loss but phase uncertainty for all ONN Mesh types
+% figure
+% plot(phase_uncert, Reck(:,1),'linewidth',2)
+% hold on
+% plot(phase_uncert, invReck(:,1),'linewidth',2)
+% plot(phase_uncert, Reck_DMM(:,1),'linewidth',2)
+% plot(phase_uncert, Reck_invReck(:,1),'linewidth',2)
+% plot(phase_uncert, Reck_DMM_invReck(:,1),'linewidth',2)
+% legend({'Reck','Inverted Reck','Reck + DMM', 'Reck + Inverted Reck','Reck + DMM + Inverted Reck'})
+% xlabel('Phase Uncertainty (\sigma)')
+% ylabel('Accuracy (%)')
+% title('0 Loss, increasing Phase Uncertainty')
+% 
+% % plot no loss but phase uncertainty for all ONN Mesh types
+% figure
+% plot(loss_dB, Reck(1,:),'linewidth',2)
+% hold on
+% plot(loss_dB, invReck(1,:),'linewidth',2)
+% plot(loss_dB, Reck_DMM(1,:),'linewidth',2)
+% plot(loss_dB, Reck_invReck(1,:),'linewidth',2)
+% plot(loss_dB, Reck_DMM_invReck(1,:),'linewidth',2)
+% legend({'Reck','Inverted Reck','Reck + DMM', 'Reck + Inverted Reck','Reck + DMM + Inverted Reck'})
+% xlabel('Loss (dB)')
+% ylabel('Accuracy (%)')
+% title('0 Phase Uncertainty, increasing loss')
--- tests/reck_loss_test.py
+++ tests/reck_loss_test.py
@@ -1,4 +1,3 @@
-
 """
 Testing the inverted reck mesh, single MZI
 Author: Simon Geoffroy-Gagnon
@@ -6,17 +5,15 @@ Edit: 07.11.2019
 """
 import pandas as pd
 from sklearn.datasets.samples_generator import make_blobs
-# import scatter_matrix_plotter as pb
-# import iris_fourth_flower as ir
 import sys
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-import pandas as pd
 import time
+
 # Set random seed to always get same data
-rng = 6
+rng = 6 
 random.seed(rng)
 
 sys.path.append('/home/simon/Documents/neuroptica')
@@ -40,110 +37,118 @@ def printf(format, *args):
 
 # Number of input features?
 N = 4
-theta =  np.pi/2
-phi =  np.pi/2
-loss = 0 # dB
 
 # X, y, Xt, yt, *_ = mnist.get_data([1,3,6,7])
-X, y, Xt, yt = blob_maker(targets=N, features=N, nsamples=5000)
+X, y, Xt, yt = blob_maker(targets=N, features=N, nsamples=1000)
 
 # Normalize inputs
 X = (X - np.min(X))/(np.max(X) - np.min(X))
-# pb.plot_blobs(X,y)
 
-losses_dB = [0.25*x for x in range(1)]# in dB
-# losses_dB = [0, 0.5]
-iterations = 1 # number of times to retry same loss
-phase_uncerts = [0] #np.linspace(0, 1.5, 31)
+losses_dB = np.linspace(0,2,2)# in dB
+iterations = 500 # number of times to retry same loss
+phase_uncerts = np.linspace(0, 2, 11) 
 
-Folder = 'Accuracy_vs_phaseUncert_300iter_100%acc/'
-setup = ['Reck','invReck','Reck+invReck','Reck+DMM+invReck']
+Folder = 'Accuracy_vs_phaseUncert_loss_500iter_100%acc/'
+setup = ['Reck','invReck','Reck+invReck','Reck+DMM+invReck', 'Reck+DMM']
 
 accuracy_per_model = []
-for idx in [0]: 
+for idx in [4]:#, 0, 1, 3, 2]: 
     print(f'model{idx}')
     accuracy = []
+    loss = 0
+    # First train the network with 0 phase noise
+    phase_uncert = 0 
+    if idx == 0:
+        model = neu.Sequential([
+            neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
+
+            # neu.AddMask(2*N),
+            # neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
+            # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
+            # neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
+
+            # neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
+
+            neu.Activation(neu.AbsSquared(N)), # photodetector measurement
+        ])
+    if idx == 1:
+        model = neu.Sequential([
+            # neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
+
+            # neu.AddMask(2*N),
+            # neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
+            # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
+            # neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
+
+            neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
+
+            neu.Activation(neu.AbsSquared(N)), # photodetector measurement
+        ])
+    if idx == 2:
+        model = neu.Sequential([
+            neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
+
+            # neu.AddMask(2*N),
+            # neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
+            # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
+            # neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
+
+            neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
+
+            neu.Activation(neu.AbsSquared(N)), # photodetector measurement
+        ])
+    if idx == 3:
+        model = neu.Sequential([
+            neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
+
+            neu.AddMask(2*N),
+            neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
+            # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
+            neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
+
+            neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
+
+            neu.Activation(neu.AbsSquared(N)), # photodetector measurement
+        ])
+    if idx == 4:
+        model = neu.Sequential([
+            neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None], phis=[None], loss=loss, phase_uncert=phase_uncert),
+
+            neu.AddMask(2*N),
+            neu.DMM_layer(2*N, thetas=[None], phis=[None], phase_uncert=phase_uncert, loss=loss),
+            # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
+            neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
+
+            # neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=thetas[-int(N*(N-1)/2):], phis=phis[-int(N*(N-1)/2):], loss=loss, phase_uncert=phase_uncert),
+
+            neu.Activation(neu.AbsSquared(N)), # photodetector measurement
+        ])
+
+    # initialize the ADAM optimizer and fit the ONN to the training data
+    optimizer = neu.InSituAdam(model, neu.MeanSquaredError, step_size=0.001)
+    losses, trn_accuracy, val_accuracy = optimizer.fit(X.T, y.T, Xt.T, yt.T, epochs=150, batch_size=2**5, show_progress=True)
+
+    # Plot loss, training acc and val acc
+    ax1 = plt.plot()
+    plt.plot(losses, color='b')
+    plt.xlabel('Epoch')
+    plt.ylabel("$\mathcal{L}$", color='b')
+    ax2 = plt.gca().twinx()
+    ax2.plot(trn_accuracy, color='r')
+    ax2.plot(val_accuracy, color='g')
+
+    plt.ylabel('Accuracy', color='r')
+    plt.legend(['Training Accuracy', 'Validation Accuracy'])
+    plt.title(f'Gradient Descent, Final Validation Accuracy: {val_accuracy[-1]:.2f}')
+    plt.ylim([0, 100])
+    plt.show()
+    # plt.savefig(f'{Folder}{setup[idx]}_loss={loss}_uncert={phase_uncert:.3f}.pdf')
+    # plt.clf()
+
+    # save a txt file containing the loss, trn acc, val acc, in case i want to replot it using matlab
+    np.savetxt(f'{Folder}{setup[idx]}_loss={loss}_uncert={phase_uncert:.3f}.txt',np.array([losses, trn_accuracy, val_accuracy]).T, delimiter=',', fmt='%.4f')
     for loss in losses_dB:
-        print(f'loss = {loss} dB')
-
-        # First train the network with 0 phase noise
-        phase_uncert = 0 
-        if idx == 0:
-            model = neu.Sequential([
-                neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
-
-                # neu.AddMask(2*N),
-                # neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
-                # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
-                # neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
-
-                # neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
-
-                neu.Activation(neu.AbsSquared(N)), # photodetector measurement
-            ])
-        if idx == 1:
-            model = neu.Sequential([
-                # neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
-
-                # neu.AddMask(2*N),
-                # neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
-                # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
-                # neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
-
-                neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
-
-                neu.Activation(neu.AbsSquared(N)), # photodetector measurement
-            ])
-        if idx == 2:
-            model = neu.Sequential([
-                neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
-
-                # neu.AddMask(2*N),
-                # neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
-                # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
-                # neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
-
-                neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
-
-                neu.Activation(neu.AbsSquared(N)), # photodetector measurement
-            ])
-        if idx == 3:
-            model = neu.Sequential([
-                neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss, phase_uncert=phase_uncert),
-
-                neu.AddMask(2*N),
-                neu.DMM_layer(2*N, thetas=[None]*int(N), phis=[None]*int(N)),
-                # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
-                neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
-
-                neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=[None]*int(N*(N-1)/2), phis=[None]*int(N*(N-1)/2), loss=loss),
-
-                neu.Activation(neu.AbsSquared(N)), # photodetector measurement
-            ])
-
-        # initialize the ADAM optimizer and fit the ONN to the training data
-        optimizer = neu.InSituAdam(model, neu.MeanSquaredError, step_size=0.001)
-        losses, trn_accuracy, val_accuracy = optimizer.fit(X.T, y.T, Xt.T, yt.T, epochs=70, batch_size=2**5, show_progress=True)
-
-        # Plot loss, training acc and val acc
-        ax1 = plt.plot()
-        plt.plot(losses, color='b')
-        plt.xlabel('Epoch')
-        plt.ylabel("$\mathcal{L}$", color='b')
-        ax2 = plt.gca().twinx()
-        ax2.plot(trn_accuracy, color='r')
-        ax2.plot(val_accuracy, color='g')
-
-        plt.ylabel('Accuracy', color='r')
-        plt.legend(['Training Accuracy', 'Validation Accuracy'])
-        plt.title(f'Gradient Descent, Final Validation Accuracy: {val_accuracy[-1]:.2f}')
-        plt.ylim([0, 100])
-        plt.savefig(f'{Folder}{setup[idx]}_loss={loss}_uncert={phase_uncert:.3f}.pdf')
-        plt.clf()
-
-
-        # save a txt file containing the loss, trn acc, val acc, in case i want to replot it using matlab
-        np.savetxt(f'{Folder}{setup[idx]}_loss={loss}_uncert={phase_uncert:.3f}.txt',np.array([losses, trn_accuracy, val_accuracy]).T, delimiter=',', fmt='%.4f')
+        # print(f'loss = {loss} dB')
 
         # Create phase array
         phases = []
@@ -158,6 +163,7 @@ for idx in [0]:
         # Now calculate the accuracy when adding phase noise and/or mzi loss
         acc_array = []
         for phase_uncert in phase_uncerts:
+            print(f'loss = {loss} dB, Phase Uncert = {phase_uncert:.2f}')
             if idx == 0:
                 model = neu.Sequential([
                     neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=thetas, phis=phis, loss=loss, phase_uncert=phase_uncert),
@@ -210,10 +216,23 @@ for idx in [0]:
 
                     neu.Activation(neu.AbsSquared(N)), # photodetector measurement
                 ])
+            if idx == 4:
+                model = neu.Sequential([
+                    neu.ReckLayer(N, include_phase_shifter_layer=False, thetas=thetas[:int(N*(N-1)/2)], phis=phis[:int(N*(N-1)/2)], loss=loss, phase_uncert=phase_uncert),
+
+                    neu.AddMask(2*N),
+                    neu.DMM_layer(2*N, thetas=thetas[int(N*(N-1)/2):int(N*(N-1)/2)+int(N)], phis=phis[int(N*(N-1)/2):int(N*(N-1)/2)+int(N)], phase_uncert=phase_uncert, loss=loss),
+                    # neu.DropMask(N=2*N, keep_ports=range(0, 2*N, 2)), # makes the DMM thetas pi
+                    neu.DropMask(N=2*N, keep_ports=range(1, 2*N+1, 2)), # makes the DMM thetas 0 or 2*pi
+
+                    # neu.flipped_ReckLayer(N, include_phase_shifter_layer=False, thetas=thetas[-int(N*(N-1)/2):], phis=phis[-int(N*(N-1)/2):], loss=loss, phase_uncert=phase_uncert),
+
+                    neu.Activation(neu.AbsSquared(N)), # photodetector measurement
+                ])
             acc = []    
 
             # calculate validation accuracy. Since phase is shifted randomly after iteration, this is good
-            for _ in range(300):
+            for _ in range(iterations):
                 Y_hat = model.forward_pass(Xt.T)
                 pred = np.array([np.argmax(yhat) for yhat in Y_hat.T])
                 gt = np.array([np.argmax(tru) for tru in yt])
@@ -245,33 +264,6 @@ if 0:
     np.savetxt(f'{setup[idx]}_acc.txt', np.array(accuracy).T, delimiter=",", fmt='%.4f')
     np.savetxt("acc_vs_uncert+loss/losses_dB.txt",losses_dB, delimiter=',', fmt='%.4f')
     np.savetxt("acc_vs_uncert+loss/phase_uncert.txt", phase_uncerts, delimiter=',', fmt='%.4f')
-    # for i in [1]:
-        # continue 
-
-               
-        # thetas.append([item for sublist in phases for (item, _) in sublist])
-        #    phis.append([item for sublist in phases for (_, item) in sublist])
-
-        # Get final accuracy
-        
-
-        # for layer in model.layers:
-        #     if hasattr(layer, 'mesh'):
-        #         phases.append([x for x in layer.mesh.all_tunable_params()])
-        #Reck_layer_idx = [f'Reck, MZI_{mzi}' for mzi in range(int(N*(N-1)/2)+N)]
-        #DMM_layer_idx = [f'DMM, MZI_{mzi}' for mzi in range(int((N*(N-1))/2),int(N*(N-1)/2+N))]
-        #Reck_H_layer_idx = [f'Reck_H, MZI_{mzi}' for mzi in range(int(N*(N-1)/2+N),int(N*(N-1)+2*N))]
-        ## df = pd.DataFrame(np.array([thetas, phis]).T, columns=['Theta','Phi'],  index=Reck_layer_idx)
-        ## df = pd.DataFrame(np.array([thetas, phis]).T, columns=['Theta','Phi'])
-        ## df = pd.DataFrame(np.array([thetas, phis]).T, columns=['Theta','Phi'], index=Reck_layer_idx+DMM_layer_idx+Reck_H_layer_idx)
-
-        ## psrint(df.round(2))
-        #acc_mean.append(val_accuracy[-1])
-        ## print(D)
-        #np.savetxt("X.csv", X, delimiter=",")
-        #np.savetxt("y.csv", y, delimiter=",")
-        #np.savetxt("yt.csv", yt, delimiter=",")
-        #np.savetxt("Xt.csv", Xt, delimiter=",")
     if 0:            
         mean_t = np.array(thetas).mean(axis=0)
         std_t = np.array(thetas).std(axis=0)
@@ -289,4 +281,3 @@ if 0:
         print(std_p)
         print('')
     # np.savetxt("Phases.txt", phases, delimiter=",")
-        # print(D)
