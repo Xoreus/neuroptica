abda2f7de90426a43400a49194f8512b4c8aeec9
simongg <simonllpgg@gmail.com>
2020-07-14 18:37:52 -0700

    fixed bugs

--- tests/main.py
+++ tests/main.py
@@ -25,17 +25,17 @@ def init_onn_settings():
     ''' Initialize onn settings for training, testing and simulation '''
     onn = ONN_Cls.ONN_Simulation() # Required for containing training/simulation information
     onn.BATCH_SIZE = 2**6 
-    onn.EPOCHS = 600
-    onn.STEP_SIZE = 0.0015 # Learning Rate
-    onn.SAMPLES = 60 # Per Class
+    onn.EPOCHS = 400
+    onn.STEP_SIZE = 0.0025 # Learning Rate
+    onn.SAMPLES = 80 # Per Class
 
     onn.ITERATIONS = 30 # number of times to retry same loss/PhaseUncert
     onn.rng_og = 0 # starting RNG value
     onn.max_number_of_tests = 10 # Max number of retries for a single model's training (keeps maximum accuracy model)
-    onn.max_accuracy_req = 95 # (%) Will stop retrying after accuracy above this is reached
+    onn.max_accuracy_req = 40 # (%) Will stop retrying after accuracy above this is reached
 
-    onn.features = 4 # How many features? max for MNIST = 784 # Add +1 if using normalize_input()
-    onn.classes = 4 # How many classes? max for MNIST = 10
+    onn.features = 16 # How many features? max for MNIST = 784 # Add +1 if using normalize_input()
+    onn.classes = 10 # How many classes? max for MNIST = 10
     onn.N = onn.features
 
     onn.range_dB = 10
@@ -61,10 +61,10 @@ def dataset(onn, dataset='MNIST', half_square_length=2):
         onn.features = (2*half_square_length)**2
         onn.N = (2*half_square_length)**2
     elif dataset == 'FFT_PCA':
-        onn.X, onn.y, onn.Xt, onn.yt = create_datasets.FFT_MNIST_PCA(classes=classes, features=features, nsamples=onn.SAMPLES) # this gives real valued vectors as input samples
+        onn.X, onn.y, onn.Xt, onn.yt = create_datasets.FFT_MNIST_PCA(classes=onn.classes, features=onn.features, nsamples=onn.SAMPLES) # this gives real valued vectors as input samples
     return onn
 
-def normalize_dataset(onn, normalization='None', experimental=False):
+def normalize_dataset(onn, normalization='Normalized', experimental=False):
     ''' Constant Power: Sends extra power to extra channel
         Normalized: Normalize input power --> [0, onn.range_linear]
     '''
@@ -75,14 +75,19 @@ def normalize_dataset(onn, normalization='None', experimental=False):
         onn.features += 1
     elif normalization == 'Normalized':              
         # To Simply Normalize input power from [0-1]*onn.range_linear #
-        # onn.X = (onn.X - np.min(onn.X))/(np.max(onn.X) - np.min(onn.X))*onn.range_linear
-        # onn.Xt = (onn.Xt - np.min(onn.Xt))/(np.max(onn.Xt) - np.min(onn.Xt))*onn.range_linear
+        if not experimental:
+            onn.X = (onn.X - np.min(onn.X))/(np.max(onn.X) - np.min(onn.X))*onn.range_linear
+            onn.Xt = (onn.Xt - np.min(onn.Xt))/(np.max(onn.Xt) - np.min(onn.Xt))*onn.range_linear
+
         # To shift them to useable experimental samples...
         onn.range_dB = 10
         onn.dB_shift = 0.6
         if experimental:
+            # change it to dBs
             onn.X = 10*np.log10(np.abs(onn.X)**2+sorted(set(np.abs(onn.X).reshape(-1)))[1])
+            # Then normalize it ([0, 1] - shift)*range_dB
             onn.X = ((onn.X - np.min(onn.X))/(np.max(onn.X) - np.min(onn.X)) - onn.dB_shift)*onn.range_dB
+            # Then reconvert it back to linear values
             onn.X = 10**(onn.X/10)
             onn.Xt = 10*np.log10(np.abs(onn.Xt)**2+sorted(set(np.abs(onn.Xt).reshape(-1)))[1])
             onn.Xt = ((onn.Xt - np.min(onn.Xt))/(np.max(onn.Xt) - np.min(onn.Xt)) - onn.dB_shift)*onn.range_dB
@@ -108,21 +113,25 @@ def create_model(features, classes):
     ''' create ONN model based on neuroptica layer '''
     eo_settings = {'alpha': 0.1, 'g':0.5 * np.pi, 'phi_b': -1 * np.pi} # If Electro-Optic Nonlinear Activation is used
 
-    model = neu.Sequential([
     # If you want multi-layer Diamond Topology
-        neu.AddMaskDiamond(features),
-        neu.DiamondLayer(features),
-        neu.DropMask(2*features - 2, keep_ports=range(features - 2, 2*features - 2)), # Bottom Diamond Topology
-        neu.Activation(neu.cReLU(features)),
-        neu.AddMaskDiamond(features),
-        neu.DiamondLayer(features),
-        neu.DropMask(2*features - 2, keep_ports=range(features - 2, 2*features - 2)), # Bottom Diamond Topology
-        neu.Activation(neu.AbsSquared(features)), # photodetector measurement
-        neu.DropMask(features, keep_ports=range(classes)),
-    ])
+    # model = neu.Sequential([
+    #     neu.AddMaskDiamond(features),
+    #     neu.DiamondLayer(features),
+    #     neu.DropMask(2*features - 2, keep_ports=range(features - 2, 2*features - 2)), # Bottom Diamond Topology
+    #     neu.Activation(neu.cReLU(features)),
+    #     neu.AddMaskDiamond(features),
+    #     neu.DiamondLayer(features),
+    #     neu.DropMask(2*features - 2, keep_ports=range(features - 2, 2*features - 2)), # Bottom Diamond Topology
+    #     neu.Activation(neu.AbsSquared(features)), # photodetector measurement
+    #     neu.DropMask(features, keep_ports=range(classes)),
+    # ])
 
     # If you want regular Clements (multi-layer) topology
     model = neu.Sequential([
+        neu.ClementsLayer(features),
+        neu.Activation(neu.cReLU(features)),
+        neu.ClementsLayer(features),
+        neu.Activation(neu.cReLU(features)),
         neu.ClementsLayer(features),
         neu.Activation(neu.cReLU(features)),
         neu.ClementsLayer(features),
@@ -131,22 +140,22 @@ def create_model(features, classes):
     ])
 
     # If you want regular Clements (single-layer) topology
-    model = neu.Sequential([
-        neu.ClementsLayer(features),
-        neu.Activation(neu.AbsSquared(features)), # photodetector measurement
-        neu.DropMask(features, keep_ports=range(classes))
-    ])
+    # model = neu.Sequential([
+    #     neu.ClementsLayer(features),
+    #     neu.Activation(neu.AbsSquared(features)), # photodetector measurement
+    #     neu.DropMask(features, keep_ports=range(classes))
+    # ])
     return model
 
 def main():
     onn = init_onn_settings()
     np.random.seed(onn.rng)
-    onn = dataset(onn, dataset='Gauss')
-    onn = normalize_dataset(onn, normalization='None', experimental=False)
+    onn = dataset(onn, dataset='MNIST')
+    onn = normalize_dataset(onn, normalization='Normalized', experimental=True)
     model = create_model(onn.features, onn.classes)
 
     loss_diff = [0] # If loss_diff is used in insertion loss/MZI
-    loss_training = [0] # loss used during training
+    training_loss = [0] # loss used during training
 
     for lossDiff in loss_diff:
         for trainLoss in training_loss:
@@ -155,7 +164,7 @@ def main():
 
             max_acc = 0 # Reset maximum accuracy achieved
             onn.loss_diff = lossDiff
-            onn.loss_dB = [lossTrain]
+            onn.loss_dB = [trainLoss]
             for onn.rng in range(onn.max_number_of_tests):
                 onn.phases = [] # Reset Saved Phases
                 
@@ -164,7 +173,7 @@ def main():
                 current_phases = [[(None, None) for _ in layer] for layer in current_phases]
                 model.set_all_phases_uncerts_losses(current_phases)
 
-                onn, model = train.train_single_onn(onn, model, loss_function='mse') # 'cce' for complex models, 'mse' for simple single layer ONNs
+                onn, model = train.train_single_onn(onn, model, loss_function='cce') # 'cce' for complex models, 'mse' for simple single layer ONNs
 
                 # Save best model
                 if max(onn.val_accuracy) > max_acc:
@@ -176,9 +185,9 @@ def main():
                 if (max(onn.val_accuracy) > onn.max_accuracy_req or
                         onn.rng == onn.max_number_of_tests-1):
                     onn.loss_diff = lossDiff # Set loss_diff
-                    onn.loss_dB = np.linspace(0, 2, 31) # set loss/MZI range
-                    onn.phase_uncert_theta = np.linspace(0., 1, 31) # set theta phase uncert range
-                    onn.phase_uncert_phi = np.linspace(0., 1, 31) # set phi phase uncert range
+                    onn.loss_dB = np.linspace(0, 2, 3) # set loss/MZI range
+                    onn.phase_uncert_theta = np.linspace(0., 1, 3) # set theta phase uncert range
+                    onn.phase_uncert_phi = np.linspace(0., 1, 3) # set phi phase uncert range
 
                     print('Test Accuracy of validation dataset = {:.2f}%'.format(calc_acc.accuracy(onn, model, onn.Xt, onn.yt)))
 
